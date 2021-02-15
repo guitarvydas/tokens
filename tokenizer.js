5c5a9f2ae9c7760b11310b1020f94eed36d021b1
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-15 16:01:43 -0500

    made all rules lower-case, newline working now

--- tokenizer.js
+++ tokenizer.js
@@ -1,14 +1,14 @@
 // npm install ohm-js
 const grammar = `
 Tokens {
-  Tokens = Token+
-  Token = comment | string | whiteSpace | symbol | integer | character
+  tokens = token+
+  token = comment | string | whiteSpace | symbol | integer | character
   comment = percent (~newline anyChar)* newline
   string = quote (~quote anyChar)* quote
   whiteSpace = ws+
   symbol = symbolFirst symbolMore*
   integer = num+
-  character = any
+  character = newline | any
   
   percent = "%"
   newline = "\\n"
@@ -20,23 +20,25 @@ Tokens {
   anyChar = any
 }`;
 
+var line;
+var offset;
+
 function tokenize (text) {
     var ohm = require ('ohm-js');
     var parser = ohm.grammar (grammar);
     var result = parser.match (text);
     if (result.succeeded ()) {
-	console.log ("ohm matching succeeded");
 	var semantics = parser.createSemantics ();
 	addSem (semantics);
+	line = 1;
+	offset = 1;
 	var result = semantics (result).token ();
 	return result;
     } else {
-	console.log (diagramParser.trace (text).toString ());
+	console.log (parser.trace (text).toString ());
 	throw "Ohm matching failed";
     }
 }
-
-var btoa = require ('btoa');
 
 function addSem (sem) {
     line = 1;
@@ -44,17 +46,22 @@ function addSem (sem) {
     sem.addOperation (
     "token",
     {
-	Tokens: function (_1s) { return _1s.token ().join ("\n"); },
-	Token: function (_1) { return _1.token (); },
-	comment: function (_1, _2s, _3) { return `token comment ${btoa (_2s.token ().join (''))} line[${line}] pos[${offset}]`; },
-	string: function (_1, _2, _3) { return `token string ${btoa (_2s.token ().join (''))} line[${line}] pos[${offset}]`; },
+	tokens: function (_1s) { return _1s.token ().join ("\n"); },
+	token: function (_1) { return _1.token (); },
+	comment: function (_1, _2s, _3) { return `token comment ${encodeURIComponent (_2s.token ().join (''))} line[${line}] pos[${offset}]`; },
+	string: function (_1, _2, _3) { return `token string ${encodeURIComponent (_2s.token ().join (''))} line[${line}] pos[${offset}]`; },
 	whiteSpace: function (_1) { return _1.token (); },
-	symbol: function (_1, _2s) { return `token symbol ${btoa (_1.token ())}${btoa (_2s.token ().join (''))} line[${line}] pos[${offset}]`; },
-	integer : function (_1) { return `token symbol ${btoa (_1.token ())} line[${line}] pos[${offset}]`; },
-	character: function (_1) { return `token character ${btoa (_1.token ())} line[${line}] pos[${offset}]`; },
+	symbol: function (_1, _2s) { return `token symbol ${encodeURIComponent (_1.token ())}${encodeURIComponent (_2s.token ().join (''))} line[${line}] pos[${offset}]`; },
+	integer : function (_1) { return `token symbol ${encodeURIComponent (_1.token ())} line[${line}] pos[${offset}]`; },
+	character: function (_1) { return `token character ${encodeURIComponent (_1.token ())} line[${line}] pos[${offset}]`; },
 
         percent: function (_1) { offset += 1; return "%"; },
-        newline: function (_1) { offset = 1; line += 1; return "\n"; },
+        newline: function (_1) {
+	    console.log (line);
+	    console.log (offset);
+            offset = 1; 
+            line += 1; 
+            return "\n"; },
         quote: function (_1) { offset += 1; return "\""; },
         ws: function (_1) { offset += 1; return this.primitiveValue; },
         symbolFirst: function (_1) { offset += 1; return _1.token (); },
@@ -67,7 +74,7 @@ function addSem (sem) {
 }
 
 function main () {
-    var text = getJSON("-");
+    var text = getJSON("test.txt");
     var parsed = tokenize (text);
     return parsed;
 }
