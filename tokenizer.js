d7df5107e830c7033214badfbb80142238aee3b7
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-18 12:20:01 -0500

    include " in string

--- tokenizer.js
+++ tokenizer.js
@@ -50,7 +50,8 @@ function addSem (sem) {
 	token: function (_1) { return _1.token (); },
 	comment: function (_1, _2s, _3) { 
 	    return `[comment ${encodeURIComponent(_1.token ())}${encodeURIComponent (_2s.token ().join (''))}${encodeURIComponent(_3.token ())} ${line} ${offset}]`; },
-	string: function (_1, _2, _3) { return `[string ${encodeURIComponent (_2s.token ().join (''))} ${line} ${offset}]`; },
+	string: function (_1, _2s, _3) {
+	    return `[string ${encodeURIComponent(_1.token ())}${encodeURIComponent (_2s.token ().join (''))}${_3.token ()} ${line} ${offset}]`; },
 	whiteSpace: function (_1s) { return `[whiteSpace ${encodeURIComponent (_1s.token ().join (''))} ${line} ${offset}]`; },
 	symbol: function (_1, _2s) { return `[symbol ${encodeURIComponent (_1.token ())}${encodeURIComponent (_2s.token ().join (''))}  ${line} ${offset}]`; },
 	integer : function (_1) { return `[symbol ${encodeURIComponent (_1.token ())}  ${line} ${offset}]`; },
